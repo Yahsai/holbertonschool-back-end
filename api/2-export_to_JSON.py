06b30173b373251a2fa6b65f9b91924ce3219355
Yahsai <yahsaisantana1@gmail.com>
2024-04-15 17:20:23 -0400

    Task 2 update

--- api/2-export_to_JSON.py
+++ api/2-export_to_JSON.py
@@ -1,46 +1,54 @@
 #!/usr/bin/python3
+"""
+This module uses Python to make requests to a REST API.
+q
+It fetches data about a specific employee's tasks
+and prints a summary of the tasks completed and the
+titles of the completed tasks.
+"""
+import json
 import requests
 import sys
-import json
 
-def fetch_and_export_todo_list(employee_id):
-    # URL for the REST API
-    url = f"https://jsonplaceholder.typicode.com/users/{employee_id}/todos"
-    
-    # Fetching data from the API
-    response = requests.get(url)
-    
-    # Checking if the request was successful
-    if response.status_code != 200:
-        print("Failed to fetch TODO list progress.")
-        return
-    
-    todos = response.json()
-    
-    # Extracting employee name
-    employee_name = todos[0]['name'].split()[0]
-    
-    # Creating list of tasks
-    tasks = []
-    for todo in todos:
-        task_info = {
-            "task": todo['title'],
-            "completed": todo['completed'],
-            "username": employee_name
-        }
-        tasks.append(task_info)
-    
-    # Writing data to JSON file
-    filename = f"{employee_id}.json"
-    with open(filename, 'w') as f:
-        json.dump({str(employee_id): tasks}, f, indent=4)
-    
-    print(f"Data exported to {filename} successfully.")
 
-if __name__ == "__main__":
-    if len(sys.argv) != 2:
-        print("Usage: python script.py <employee_id>")
-        sys.exit(1)
-    
-    employee_id = int(sys.argv[1])
-    fetch_and_export_todo_list(employee_id)
+# Get the employee id
+employee_id = sys.argv[1]
+# Set the url and get a respnse
+user_response = requests.get(
+    f'https://jsonplaceholder.typicode.com/users/{employee_id}')
+# Get data in json form
+
+data = user_response.json()
+# Get the name of the employee
+employee_name = data['name']
+
+# Get the todo data fro the API
+todos_response = requests.get(
+    f'https://jsonplaceholder.typicode.com/todos?userId={employee_id}')
+# Get the data in json form
+todos_data = todos_response.json()
+
+# Get the total number of tasks
+total_todos = len(todos_data)
+# Get the number of completed tasks
+ok_todos = sum(1 for task in todos_data if task['completed'])
+
+# Print the first line of the output
+print(
+    f'Employee {employee_name} is done with tasks({ok_todos}/{total_todos}):')
+
+# Print the title of each completed task
+for task in todos_data:
+    if task['completed']:
+        print('\t ' + task['title'])
+
+# Export using JSON Format
+with open('USER_ID.json', 'w') as jsonfile:
+    json.dump({employee_id: [{
+        "task": task['title'],
+        "completed": task['completed'],
+        "username": employee_name
+    } for task in todos_data]}, jsonfile)
+
+if __name__ == '__main__':
+    pass
